e286e3dd5575b8f5eb6a434fd206e68ea6e040a1
uraniumrainbow <uraniumrainbow@gmail.com>
2023-07-24 19:33:01 -0600

    javascrypt

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,52 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-$(function () {
+$(document).ready(function () {
+  $(".saveBtn").on("click", function () {
+    var value = $(this).siblings(".descritpion").val();
+    var time = $(this).parent().attr("id");
+
+    localStorage.setItem(time, value);
+
+    $(".notification").addClass("show");
+
+    setTimeout(function () {
+      $(".notification").removeClass("show");
+    }, 5000);
+  });
+
+  function timeUpdate() {
+    var hour = dayjs().hour();
+
+    $('.time-block').each(function () {
+      var hourBlock = parseInt($(this).attr("id").split("-")[1]);
+
+      if (hourBlock < hour) {
+        $(this).addClass("past");
+      } else if (hourBlock === hour) {
+        $(this).removeClass("past");
+        $(this).addClass("present");
+      } else {
+        $(this).removeClass("past");
+        $(this).removeClass("present");
+        $(this).addClass("future");
+      }
+    });
+  }
+
+  timeUpdate();
+
+  setInterval(timeUpdate, 10000);
+
+  $("#hour-9 .description").val(localStorage.getItem("hour-9"));
+  $("#hour-10 .description").val(localStorage.getItem("hour-10"));
+  $("#hour-11 .description").val(localStorage.getItem("hour-11"));
+  $("#hour-12 .description").val(localStorage.getItem("hour-12"));
+  $("#hour-1 .description").val(localStorage.getItem("hour-1"));
+  $("#hour-2 .description").val(localStorage.getItem("hour-2"));
+  $("#hour-3 .description").val(localStorage.getItem("hour-3"));
+  $("#hour-4 .description").val(localStorage.getItem("hour-4"));
+  $("#hour-5 .description").val(localStorage.getItem("hour-5"));
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -20,4 +65,4 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
-});
+  });
